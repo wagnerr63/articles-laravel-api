304fe52b17e4732f96461d1a8aaaac6532afb807
Wagner <wagner@hangar.digital>
2020-11-29 19:21:49 -0300

    Projeto finalizado

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Use App\Article;
+ 
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
+
+?>
